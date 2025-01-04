809db127d93bd0896a499f086708d09cdb9b0114
luizguilh3rme <luizguilhermelg107@gmail.com>
2025-01-04 19:18:35 -0300

    att

--- ControleEstoque/obj/Debug/net8.0/ControleEstoque.AssemblyInfo.cs
+++ ControleEstoque/obj/Debug/net8.0/ControleEstoque.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControleEstoque")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+172cb1e670e78a727e5f9275902da30ea7dc657b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45a44d783b1a72c782f88be459d05092b3a0f619")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControleEstoque")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControleEstoque")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
